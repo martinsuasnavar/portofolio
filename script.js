b75e2dbdd5e779942a4f67291845d839003e3feb
Martin Suasnavar <msuasnavar21@gmail.com>
2023-09-24 19:15:00 -0300

    1.0 Version, additional fetaures or corrections are desired

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+//////////////////
+//////////////////
+////JavaScript////
+//////////////////
+//////////////////
+
+
+
+
+
+///////////
+//BUTTONS//
+///////////
+
 //selecting buttons
 const myNavigatorButtons = document.getElementsByClassName("navigator-button");
 const myFacadeAboutMeButton = document.getElementById("facade_about_me_button");
@@ -6,7 +20,7 @@ const myCVDownloadButton = document.getElementById("cv_download_button")
 
 function alertNoButtonFunctionality(event){
     event.preventDefault(); //prevent the default behavior of anchor (<a>) tags
-    alert("This button has not functionality yet");
+    alert("This button has no functionality yet");
 }
 
 //event listener, needed to create alerts for buttons that still need proper functionality
@@ -27,3 +41,22 @@ myCVDownloadButton.addEventListener("click", function(event){
     alertNoButtonFunctionality(event);
 });
 
+
+
+
+//////////////
+//ANIMATIONS//
+//////////////
+
+function fullOpacityAfterAnimation(selector, time){
+    const box = document.querySelector(selector);
+
+    //after a delay, hide element again with animation
+    setTimeout(function(){
+        box.style.opacity = 1;
+    }, time);
+}
+
+fullOpacityAfterAnimation("#facade", 200);
+fullOpacityAfterAnimation(".facade-item", 1000);
+fullOpacityAfterAnimation(".facade-button", 1000);
